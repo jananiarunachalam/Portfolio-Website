9f319e7b804860823e23db1fe8337bd73b2bb7db
jananiarunachalam <jananiarunachalam17@gmail.com>
2020-09-23 01:04:53 -0400

    navbar

--- src/canvas/navbar/index.js
+++ src/canvas/navbar/index.js
@@ -1,8 +1,41 @@
 import React from "react";
-import { Toolbar } from "@material-ui/core";
+import { Toolbar, Avatar, Typography } from "@material-ui/core";
+import NavLink from "./navlink";
+import { makeStyles } from "@material-ui/core";
+
+const useStyle = makeStyles((theme) => ({
+  root: {},
+  title: {
+    marginLeft: theme.spacing(1),
+  },
+  fillSpace: {
+    flexGrow: 1,
+  },
+  small: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+    marginLeft: theme.spacing(1),
+  },
+}));
 
 const NavBar = () => {
-  return <Toolbar>Dummy text</Toolbar>;
+  const classes = useStyle();
+  return (
+    <Toolbar>
+      <Avatar alt="Janani Arunachalam" />
+      <Typography classes={{ root: classes.title }}>
+        Janani Arunachalam
+      </Typography>
+      <div className={classes.fillSpace}></div>
+      <NavLink url="#" label="about" active />
+      <NavLink url="#" label="projects" />
+      <NavLink url="#" label="skills" />
+      <NavLink url="#" label="experience" />
+      <Avatar alt="Github" className={classes.small} />
+      <Avatar alt="LinkedIn" className={classes.small} />
+      <Avatar alt="Mail" className={classes.small} />
+    </Toolbar>
+  );
 };
 
 export default NavBar;
